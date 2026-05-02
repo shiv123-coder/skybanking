2017d5a6a136553957bfd45e9f67ebf515004af9
Shiv_Core <shivashankrmali7@gmail.com>
2026-05-02 15:54:46 +0530

    Fix servlet mapping via web.xml (remove annotation dependency)

--- src/main/java/com/skybanking/web/PingServlet.java
+++ src/main/java/com/skybanking/web/PingServlet.java
@@ -1,14 +1,10 @@
 package com.skybanking.web;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
-@WebServlet("/ping")
 public class PingServlet extends HttpServlet {
 
     @Override
